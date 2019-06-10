da52d191e38ac602593138474d4784f8e15fcae5
Sukkin Pang <skp@skpang.co.uk>
2019-06-10 20:00:58 +0100

    Update Teensy32_with_MCP2517FD_CAN_FD_demo.ino

--- Teensy32_with_MCP2517FD_CAN_FD_demo.ino
+++ Teensy32_with_MCP2517FD_CAN_FD_demo.ino
@@ -23,7 +23,7 @@ volatile uint32_t can_msg_count = 0;
 
 extern uint8_t txd[MAX_DATA_BYTES];
 extern CAN_TX_MSGOBJ txObj;
-
+int led = 13;
 #define OLED_RESET 5
 Adafruit_SSD1306 display(OLED_RESET);
 #define LOGO16_GLCD_HEIGHT 16 
@@ -34,7 +34,7 @@ Adafruit_SSD1306 display(OLED_RESET);
 
 
 void setup() {
-
+ //pinMode(led, OUTPUT);   
   delay(1000);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3D);  // initialize with the I2C addr 0x3D (for the 128x64)
   display.display();
@@ -46,57 +46,49 @@ void setup() {
   display.println(" ");
   display.println(" skpang.co.uk");
   display.println(" ");
-  display.println(" 121/17");
+  display.println(" 06/19");
   display.display();
       
   Serial.println(F("CAN Bus Tx test"));
  
   APP_CANFDSPI_Init(CAN_500K_2M);       // 500k arbitration rate and 2Mbps data rate
-  // APP_CANFDSPI_Init(CAN_500K_4M);       // 500k arbitration rate and 2Mbps data rate
- //  APP_CANFDSPI_Init(CAN_500K_8M);       // 500k arbitration rate and 2Mbps data rate
-
-
-  
+  // APP_CANFDSPI_Init(CAN_500K_4M);       // 500k arbitration rate and 4Mbps data rate
+  // APP_CANFDSPI_Init(CAN_500K_8M);       // 500k arbitration rate and 8Mbps data rate
   
   txObj.bF.ctrl.IDE = 0;      // Extended CAN ID false
   txObj.bF.id.SID = 0x7df;    // CAN ID
+
   txObj.bF.ctrl.BRS = 1;      // Switch Bitrate true (switch to 2Mbps)
   txObj.bF.ctrl.FDF = 1;      // CAN FD true
-  txObj.bF.ctrl.DLC = 15;      // Data length
-txd[4] = can_msg_count;
-  delay(10);
-   APP_TransmitMessageQueue();
-   
+  txObj.bF.ctrl.DLC = 15;     // Data length. 15 is 64 bytes
+
+  for(int i = 0;i <64;i++)
+  {
+    txd[i] = 0;
+  }
+ 
+     
 }
 
 
-void loop() {
+void loop() 
+{
 
-
-//APP_TransmitMessageQueue(); // Send out CAN FD frame
-
- txd[4] = can_msg_count >> 24;
+  txd[4] = can_msg_count >> 24;
   txd[5] = can_msg_count >> 16;
- txd[6] = can_msg_count >> 8;
- txd[7] = can_msg_count;
+  txd[6] = can_msg_count >> 8;
+  txd[7] = can_msg_count;
 
- // can_msg_count++;
-  
- 
+  can_msg_count++;
+
+  APP_TransmitMessageQueue(); // Send out CAN FD frame
 
   if(digitalReadFast(int1_pin) == 0)  // Read int1 pin on MCP2517FD
   {
     // It is low read out the data
     APP_ReceiveMessage_Tasks();
-    
   }
- 
-
+    
+  delay(100); 
   
 }
-
-
-
-
-
-
